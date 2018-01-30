f6bd88f0077f289db9016d0521e09122697985ae
shovelware <davy.whelan@live.co.uk>
2018-01-30 09:26:30 +0100

    Starter commit

---
+++ src/main/java/nl/cerios/scoop/ScoopApplication.java
@@ -0,0 +1,12 @@
+package nl.cerios.scoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoopApplication.class, args);
+	}
+}
---
+++ src/test/java/nl/cerios/scoop/ScoopApplicationTests.java
@@ -0,0 +1,16 @@
+package nl.cerios.scoop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScoopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
