1fb2af328775e41167fe91defac79c2af37c3a90
shovelware <davy.whelan@live.co.uk>
2018-02-08 15:41:26 +0100

    Added rough Film DB and object, display fragments and pages.

--- src/main/java/nl/cerios/scoop/ScoopApplication.java
+++ src/main/java/nl/cerios/scoop/ScoopApplication.java
@@ -3,22 +3,63 @@ package nl.cerios.scoop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import java.util.ArrayList;
+import java.util.Locale;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @Controller
 public class ScoopApplication {
+
+	static FilmDB db;
+
 	//Main
 	public static void main(String[] args) {
 		SpringApplication.run(ScoopApplication.class, args);
+		db = new FilmDB();
 	}
 
+	//Logic
     @RequestMapping(value = "/index")
-    public String index() { return "index"; }
+    public String index() {
+		//logic.films ophalen(vandaag)
+		//
+		//get
+
+		return "index";
+	}
+
+	@RequestMapping(value = "/films")
+	public String films(Model model) {
+		//Get all films from db
+		//add films to model
+		//redirect to film page
+
+        ArrayList<Film> films = db.getFilms();
+        model.addAttribute("films", films);
+
+		return "filmsview";
+	}
+
+	@RequestMapping(value ="/film")
+	public String film(@RequestParam(value="id", required=false, defaultValue="0") String filmId, Model model) {
+
+	    Film f = db.getFilm(filmId);
+
+	    model.addAttribute("film", f);
+
+		return "filmview";
+	}
 
 	@RequestMapping(value = "/login")
 	public String login() {
@@ -32,8 +73,13 @@ public class ScoopApplication {
 
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+        //logic.dostuff
+		//data.getdata
+		//model.addData
+		//return templatename
+
+		model.addAttribute("name", name);
+        return "greeter";
     }
 
 	//Testing Functions
--- src/test/java/nl/cerios/scoop/ScoopApplicationTests.java
+++ src/test/java/nl/cerios/scoop/ScoopApplicationTests.java
@@ -11,6 +11,7 @@ public class ScoopApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
 	}
 
 }
