493a72f8e49c228a13aa4756de6b5240320d31b0
shovelware <davy.whelan@live.co.uk>
2018-02-13 10:04:42 +0100

    Reorganised film code into services and controllers.

--- src/main/java/nl/cerios/scoop/ScoopApplication.java
+++ src/main/java/nl/cerios/scoop/ScoopApplication.java
@@ -1,109 +1,44 @@
 package nl.cerios.scoop;
 
+import nl.cerios.scoop.service.FilmService;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.View;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
-import java.util.ArrayList;
-import java.util.Locale;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@Controller
-public class ScoopApplication {
+public class ScoopApplication extends SpringBootServletInitializer {
 
-	static FilmDB db;
+	static FilmService db;
 
 	//Main
 	public static void main(String[] args) {
 		SpringApplication.run(ScoopApplication.class, args);
-		db = new FilmDB();
+		db = new FilmService();
 	}
 
-	//Logic
-    @RequestMapping(value = "/index")
-    public String index() {
-		//logic.films ophalen(vandaag)
-		//
-		//get
-
-		return "index";
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ScoopApplication.class);
 	}
 
-	@RequestMapping(value = "/films")
-	public String films(Model model) {
-		//Get all films from db
-		//add films to model
-		//redirect to film page
-
-        ArrayList<Film> films = db.getFilms();
-        model.addAttribute("films", films);
-
-		return "filmsview";
-	}
-
-	@RequestMapping(value ="/film")
-	public String film(@RequestParam(value="id", required=false, defaultValue="0") String filmId, Model model) {
-
-	    Film f = db.getFilm(filmId);
-
-	    model.addAttribute("film", f);
-
-		return "filmview";
-	}
-
-	@RequestMapping(value = "/login")
-	public String login() {
-	    return "login";
-    }
-
-    @RequestMapping(value = "/logout")
-    public String logout() {
-	    return "logout";
-    }
-
-    @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        //logic.dostuff
-		//data.getdata
-		//model.addData
-		//return templatename
-
-		model.addAttribute("name", name);
-        return "greeter";
-    }
-
-	//Testing Functions
-	@RequestMapping(value = "/templateloc", method = RequestMethod.GET)
-	@ResponseBody
-	public String locateTemplates() {
-		return "1";
-	}
-
-	@RequestMapping(value = "/version", method = RequestMethod.GET)
-	@ResponseBody
-	public String version() {
-		return "0";
-	}
-
-	@GetMapping(value = "/test/GET")
-	@ResponseBody
-	public String getResponse() {
-		return "You GETted the server!";
-	}
-
-	@PostMapping(value = "/test/POST")
-	@ResponseBody
-	public String postResponse() {
-		return "You POSTed the server!";
-	}
+//	public String films(Model model) {
+//		//Get all films from db
+//		//add films to model
+//		//redirect to film page
+//
+//        ArrayList<Film> films = db.getFilms();
+//        model.addAttribute("films", films);
+//
+//		return "filmsview";
+//	}
+//
+//	public String film(@RequestParam(value="id", required=false, defaultValue="0") String filmId, Model model) {
+//
+//	    Film f = db.getFilm(filmId);
+//
+//	    model.addAttribute("film", f);
+//
+//		return "filmview";
+//	}
 }
